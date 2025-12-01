d11105dc78000b8c552b62c57e69fb6c4bd89e98
groknut <184639205+groknut@users.noreply.github.com>
2025-12-01 15:48:08 +0800

    update compile|run

--- compile.bat
+++ compile.bat
@@ -14,9 +14,9 @@ if %errorlevel% neq 0 (
 )
 
 echo %time% 
-uv run create-map.py ./input/karta-01.bmp
+uv run ./py/create-map.py ./input/karta-01.bmp
 
 echo %time%
 .\main.exe config.ic
 echo %time%
-uv run main.py
+uv run ./py/generate-map.py ./input/karta-01.bmp
--- run.bat
+++ run.bat
@@ -6,9 +6,9 @@
 echo run project...
 
 echo %time% 
-uv run create-map.py ./input/karta-01.bmp
+uv run ./py/create-map.py ./input/karta-01.bmp
 
 echo %time%
 .\main.exe config.ic
 echo %time%
-uv run main.py
+uv run ./py/generate-map.py ./input/karta-01.bmp
