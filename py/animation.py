1893b32280218e8f3b354f9d0c3ea5cd7a4f82b8
groknut <184639205+groknut@users.noreply.github.com>
2025-12-03 22:21:07 +0800

    add py script for animation

---
+++ py/animation.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as pp
+import numpy as np
+import sys
+from matplotlib.animation import FuncAnimation
+
+def load_points(coords):
+    with open(coords, 'r') as f:
+    	points = np.array([list(map(int, line.split())) for line in f if line.strip()])
+    return points
+
+def animate_path(image, coords):
+
+    img = pp.imread(image)
+    points = load_points(coords)
+
+    fig, ax = pp.subplots()
+    ax.imshow(img)
+
+    line, = ax.plot([], [], 'r-', lw=2)
+    dot, = ax.plot([], [], 'blue', ms=6)
+
+    for x, y in points:
+        ax.add_patch(
+            pp.Circle((x, y), 4, color="blue", fill=True)
+        )
+
+    def anim(frame):
+        if frame < len(points):
+            line.set_data(points[:frame+1, 0], points[:frame+1, 1])
+            dot, = ax.plot([], [], 'blue', ms=8)
+        return line
+
+    dot.set_data([], [])
+    ani = FuncAnimation(fig, anim, frames=len(points),
+    init_func=lambda: (line.set_data([], []), dot.set_data([], [])),
+    interval=200)
+
+    return ani
+
+
+def main():
+    if len(sys.argv) != 3:
+        print(f"""
+Usage: python {sys.argv[0]} <input_map_file> <file_coords>
+Example: python {sys.argv[0]} ./input/karta-01.bmp ./output/coords.txt
+        """)
+        sys.exit(0)
+    else:
+        ani = animate_path(sys.argv[1], sys.argv[2])
+        pp.show()
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
