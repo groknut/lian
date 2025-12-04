ef666672ba5d879e23f2397794bd85dad4790b40
groknut <184639205+groknut@users.noreply.github.com>
2025-12-04 15:33:19 +0800

    update py config

--- py/config.py
+++ py/config.py
@@ -1,5 +1,16 @@
 
 import numpy as np
+import functools
+import time
+
+def timeit(func):
+	@functools.wraps(func)
+	def wrapper(*args, **kwargs):
+		start = time.perf_counter()
+		result = func(*args, **kwargs)
+		print(f"{func.__name__}: {time.perf_counter() - start:.4f}с")
+		return result
+	return wrapper
 
 def load_points(coords):
     with open(coords, 'r') as f:
