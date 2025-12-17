e9ea43cd1c5a1da4d078a853f06bd5a386921f47
groknut <184639205+groknut@users.noreply.github.com>
2025-12-17 16:52:17 +0800

    update py script for compile/start

--- py/start_cpp.py
+++ py/start_cpp.py
@@ -12,26 +12,26 @@ def compile_cpp(output_file):
     exe_path = str(Path(output_file).with_suffix('.exe'))  if os.name == "nt" else str(Path(output_file))
 
     if not os.path.exists(exe_path):
-        sp.run(f"g++ main.cpp src/*.cpp -o {output_file}", shell=True)
+        sp.run(f"g++ -Ilibs/cfig  main.cpp src/*.cpp libs/cfig/cfig.cpp -o main", shell=True)
 
 @timeit
-def start_cpp(output_file, config_file):
+def start_cpp(output_file):
 
     exe_path = str(Path(output_file).with_suffix('.exe'))  if os.name == "nt" else str(Path(output_file))
     sp.run(
-        f"{output_file} {config_file}", shell=True
+        f"{output_file}", shell=True
     )
 
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print(f"""
-Usage: python {sys.argv[0]} <output_file> <config_file>
-Example: python {sys.argv[0]} main config.ic
+Usage: python {sys.argv[0]} <output_file>
+Example: python {sys.argv[0]} main
         """)
         sys.exit(0)
     else:
         compile_cpp(sys.argv[1])
-        start_cpp(sys.argv[1], sys.argv[2])
+        start_cpp(sys.argv[1])
         sys.exit(0)
 
 if __name__ == "__main__":
